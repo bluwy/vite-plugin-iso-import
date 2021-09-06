e639c9beeec6681f6b8af80fda014470eb325949
bluwy <bjornlu.dev@gmail.com>
2021-09-06 15:32:51 +0800

    Fix prebundling

--- src/index.js
+++ src/index.js
@@ -12,6 +12,15 @@ export function isoImport() {
   return {
     name: 'vite-plugin-iso-import',
     enforce: 'post',
+    config() {
+      return {
+        optimizeDeps: {
+          esbuildOptions: {
+            plugins: [esbuildPatchPlugin()]
+          }
+        }
+      }
+    },
     async transform(code, id, ssr) {
       await init
 
@@ -44,6 +53,66 @@ export function isoImport() {
           code: _s.toString(),
           map: _s.generateMap()
         }
+      }
+    }
+  }
+}
+
+/**
+ * I wish I would never have to write this again :(
+ *
+ * This esbuild plugin patches Vite's dep scan plugin so it's `build.onResolve`
+ * callback param always receives `args.path` without `?client` or `?server` suffix.
+ *
+ * But why this method? A list of alternatives tried:
+ *
+ * 1. Esbuild plugin which resolves "my-lib?client" => "my-lib"
+ *
+ * This does not work because `build.onResolve` doesn't fallthrough
+ * (needs resolve to absolute path), which also means Vite's scanner won't catch this.
+ *
+ * 2. Use Vite plugin `resolveId` for "my-lib?client" => "my-lib?client"
+ *
+ * Almost worked, but Vite prebundles as "my-lib?client", causing actual import to
+ * trigger dynamic pre-bundling.
+ *
+ * 3. Intercept server._optimizeDepsMetadata (Idea: https://github.com/antfu/vite-plugin-optimize-persist)
+ *
+ * Too risky with potential pitfalls. Plus we need to fix this for non-package imports
+ * as well so Vite crawls into the import path, e.g. relative or alias imports.
+ *
+ * 4. Use esbuild tsconfig option to map "*?client" => "*" naively
+ *
+ * Doesn't work. Don't think it's valid either.
+ *
+ * 5. Fix Vite directly to strip anything after "?"
+ *
+ * There could be legitimate reasons we don't want that to happen.
+ *
+ * @return {import('vite').UserConfig['optimizeDeps']['esbuildOptions']['plugins'][number]}
+ */
+function esbuildPatchPlugin() {
+  return {
+    name: 'esbuild-plugin-iso-import',
+    setup(build) {
+      const viteScanPlugin = build.initialOptions.plugins.find((v) => v.name === 'vite:dep-scan')
+
+      if (!viteScanPlugin) return
+
+      const oriSetup = viteScanPlugin.setup.bind(viteScanPlugin)
+      viteScanPlugin.setup = function (build) {
+        const oriResolve = build.onResolve.bind(build)
+        build.onResolve = function (options, callback) {
+          function wrapCallback(args) {
+            args.path = args.path
+              .replace(clientRE, (_, $1, $2) => ($2 ? $1 : ''))
+              .replace(serverRE, (_, $1, $2) => ($2 ? $1 : ''))
+            return callback(args)
+          }
+
+          return oriResolve(options, wrapCallback)
+        }
+        return oriSetup(build)
       }
     }
   }
